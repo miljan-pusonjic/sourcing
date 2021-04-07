d8e4d07d9a6ec67254040516931b78493d9ba902
miljan <miljanp1991@gmail.com>
2021-04-07 17:30:32 +0200

    blog

--- blog.php
+++ blog.php
@@ -1,7 +1,42 @@
 <?php include "partials/header.php"; ?>
 
+<!-- naslov -->
+<section id="naslov">
+    <h1 class="">Blog</h1>
+</section>
 
-<h1>blog</h1>
+<!-- blog -->
+<section id="blog" class="container">
+<div class="row">
+        <div class="col-md-4">
+            <div class="card">
+                <img src="img/blog1.jpg" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h4>April 07. 2021. | Admin</h4>
+                    <h3>Digital marketing</h3>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-4">
+            <div class="card"">
+                <img src="img/blog2.jpg" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h4>April 07. 2021. | Admin</h4>
+                    <h3>Web design</h3>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-4">
+            <div class="card">
+                <img src="img/blog3.jpg" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h4>April 07. 2021. | Admin</h4>
+                    <h3>Project menagment</h3>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
 
 
 <?php include "partials/footer.php"; ?>
